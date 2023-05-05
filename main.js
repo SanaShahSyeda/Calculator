842f12b9df92cd21e6e440dae41bb1c2010b9eba
SanaShahSyeda <132450783+SanaShahSyeda@users.noreply.github.com>
2023-05-04 22:39:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function clear(){
+   document.getElementById("result").value="";
+}
+function display(val){
+  document.getElementById("result").value+=val;
+}
+function equate(){
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value=y;
+}
